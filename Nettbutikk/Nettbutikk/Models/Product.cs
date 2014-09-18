8988cf199071c06f1af00240d5279752c689578f
ik Haider Forsen <erik@forsen.no>
2014-09-18 17:27:28 +0200

    Lagt til visningsnavn

--- Nettbutikk/Nettbutikk/Models/Product.cs
+++ Nettbutikk/Nettbutikk/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,17 @@ namespace Nettbutikk.Models
 {
     public class Product
     {
+        [Display(Name = "Varenummer")]
         public int itemnumber { get; set; }
+        [Display(Name = "Produktnavn")]
         public String name { get; set; }
+        [Display(Name = "Beskrivelse")]
         public String description { get; set; }
+        [Display(Name = "Pris")]
         public int price { get; set; }
+        [Display(Name = "Produsent")]
         public String producer { get; set; }
+        [Display(Name = "Kategori")]
         public String category { get; set; }
     }
 }
