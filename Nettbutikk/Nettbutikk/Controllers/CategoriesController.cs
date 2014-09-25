c7fc48314205d153b51416d8615b720fdb08abe8
snuskeerlend <erlendrog@gmail.com>
2014-09-25 12:00:19 +0200

    Fjernet doble klasser og litt andre ting

---
+++ Nettbutikk/Nettbutikk/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nettbutikk.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        
+        // GET: Cat
+        private Models.DatabaseContext db = new Models.DatabaseContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /*
+        public ActionResult ListProducts()
+        {
+            var db = new DBProduct();
+            List<Product> listOfProducts = db.getAll();
+            return View(listOfProducts);
+        }
+        */
+
+        public ActionResult Categories()
+        {
+            List<Models.Category> allCategories = db.Categories.ToList();
+            ViewData.Model = allCategories;
+            return View();
+        }
+    }
+}
