ee32bf0512720d3f7800d60d3a3259943a08c1be
snuskeerlend <erlendrog@gmail.com>
2014-09-25 15:21:15 +0200

    ..

--- Nettbutikk/Nettbutikk/App_Start/RouteConfig.cs
+++ Nettbutikk/Nettbutikk/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Nettbutikk
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Mail", action = "Frontpage", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "ListProducts", id = UrlParameter.Optional }
             );
         }
     }
