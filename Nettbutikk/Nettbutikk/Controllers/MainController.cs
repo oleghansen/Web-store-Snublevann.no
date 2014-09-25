fb22d3327f553508e36ec1164c11fff6b2393a4c
snuskeerlend <erlendrog@gmail.com>
2014-09-25 15:17:22 +0200

    Prøv nå Ole

---
+++ Nettbutikk/Nettbutikk/Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nettbutikk.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        //endre
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
