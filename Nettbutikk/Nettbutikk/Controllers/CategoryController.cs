7368260c955511011c2d26a1e7e25300ddc71364
snuskeerlend <erlendrog@gmail.com>
2014-09-26 16:13:18 +0200

    Opprettet en klasse for Kategori og la til get, set

--- Nettbutikk/Nettbutikk/Controllers/CategoryController.cs
+++ Nettbutikk/Nettbutikk/Controllers/CategoryController.cs
@@ -6,13 +6,30 @@ using System.Web.Mvc;
 
 namespace Nettbutikk.Controllers
 {
-    public class CategoryController : Controller
+    public class CategoriesController : Controller
     {
-        // GET: Category
-        //endre
+
+        // GET: Cat
+        private Models.DatabaseContext db = new Models.DatabaseContext();
         public ActionResult Index()
         {
             return View();
         }
+
+        /*
+        public ActionResult ListProducts()
+        {
+            var db = new DBProduct();
+            List<Product> listOfProducts = db.getAll();
+            return View(listOfProducts);
+        }
+        
+        public ActionResult Categories()
+        {
+            List<Models.Category> allCategories = db.Categories.ToList();
+            ViewData.Model = allCategories;
+            return View();
+        }
+        */ 
     }
 }
---
+++ Nettbutikk/Nettbutikk/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nettbutikk.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public Product product { get; set; }
+    }
+}
