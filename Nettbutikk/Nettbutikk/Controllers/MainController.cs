ba8c2871c239b75910820bd7561bfdbd90c823ca
snuskeerlend <erlendrog@gmail.com>
2014-09-26 09:46:26 +0200

    Litt endring i css, lagt til main controller, frontpage og endret i _layout

--- Nettbutikk/Nettbutikk/Controllers/MainController.cs
+++ Nettbutikk/Nettbutikk/Controllers/MainController.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Nettbutikk.Models;
 
 namespace Nettbutikk.Controllers
 {
     public class MainController : Controller
     {
-        // GET: Main
-        //endre
-        public ActionResult Index()
+        // GET: Product
+        public ActionResult ListProducts()
+        {
+            var db = new DBProduct();
+            List<Product> listOfProducts = db.getAll();
+            return View(listOfProducts);
+        }
+
+        public ActionResult addProduct()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult addProduct(Product inProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                var productDb = new DBProduct();
+                bool insertOK = productDb.add(inProduct);
+
+                if (insertOK)
+                {
+                    return RedirectToAction("ListProducts");
+                }
+            }
+            return View();
+        }
+
     }
 }
