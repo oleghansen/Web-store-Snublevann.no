35e3d5c214651fed896f0e999701d3345aad3d5d
JulieHillRoa <julie.hill.roa@hotmail.com>
2014-09-29 12:52:16 +0200

    Endrer på strukturen til login

--- Nettbutikk/Nettbutikk/Models/Customer.cs
+++ Nettbutikk/Nettbutikk/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,30 @@ namespace Nettbutikk.Models
     public class Customer
     {
         public int id { get; set; }
+        [Display(Name = "Fornavn")]
+        [Required(ErrorMessage = "Fornavn må oppgis")]
         public String firstname { get; set; }
+        [Display(Name = "Etternavn")]
+        [Required(ErrorMessage = "Etternavn må oppgis")]
         public String lastname { get; set; }
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email må oppgis")]
         public String email { get; set; }
+         [Display(Name = "Telefonnummer")]
         public String phonenumber { get; set; }
+        [Display(Name = "Adresse")]
         public String address { get; set; }
+        [Display(Name = "Postnummer")]
         public int postalcode { get; set; }
+        [Display(Name = "Poststed")]
         public Postalareas postalarea { get; set; }
+        [Display(Name = "Brukernavn")]
+        [Required(ErrorMessage = "Brukernavn må oppgis")]
+        public String usename { get; set; }
+        [Display(Name = "Passord")]
+        [Required(ErrorMessage = "Passord må oppgis")]
+        public String password { get; set; }
+
 
     }
 }
