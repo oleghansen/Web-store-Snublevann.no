8cdae09f58f4fadc9b768cab164e1e8e1246f3c4
JulieHillRoa <julie.hill.roa@hotmail.com>
2014-09-29 13:47:51 +0200

    kundebehandling

---
+++ Nettbutikk/Nettbutikk/Controllers/CostumerContoller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nettbutikk.Controllers
+{
+    public class CostumerContoller : Controller
+    {
+        private Models.DatabaseContext db = new Models.DatabaseContext();
+
+        public ActionResult RegisterView()
+        {
+            return View();
+        }
+    }
+}
+
+
