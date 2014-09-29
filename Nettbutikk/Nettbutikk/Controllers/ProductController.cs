7cdedacd3a173f67a8fa05aec8604839aede6255
Erik Haider Forsén <erik@forsen.no>
2014-09-29 16:15:42 +0200

    Laget detaljert visning av produkt

--- Nettbutikk/Nettbutikk/Controllers/ProductController.cs
+++ Nettbutikk/Nettbutikk/Controllers/ProductController.cs
@@ -38,5 +38,11 @@ namespace Nettbutikk.Controllers
             return View();
         }
 
+        public ActionResult viewProduct(int id)
+        {
+            var db = new DBProduct();
+            return View(db.get(id));
+        }
+
     }
 }
