3641a77bfcbbef40006513eb2c6f58b5d37da5ca
JulieHillRoa <julie.hill.roa@hotmail.com>
2014-09-29 14:46:57 +0200

    Enda mer innlogging

--- Nettbutikk/Nettbutikk/Controllers/CostumerContoller.cs
+++ Nettbutikk/Nettbutikk/Controllers/CostumerContoller.cs
@@ -3,16 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Nettbutikk.Models;
 
 namespace Nettbutikk.Controllers
 {
     public class CostumerContoller : Controller
     {
-        private Models.DatabaseContext db = new Models.DatabaseContext();
-
-        public ActionResult RegisterView()
+        //hvilken side skal man skjekke at man er logget inn eller ei ?
+        //tatt fra lærern
+        public ActionResult Frontpage()
         {
-            return View();
+            if (Session["LoggetInn"] == null)
+            {
+                Session["LoggetInn"] = false;
+                ViewBag.Innlogget = false;
+            }
+            else
+            {
+                ViewBag.Innlogget = (bool)Session["LoggetInn"];
+            }
+             return View();
+         }
+
+        [HttpPost]
+        public ActionResult Register(Customer newUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            using (var db = new Models.DatabaseContext())
+            {
+                try
+                {
+                    var user = new Customers();
+                    byte[] hasedpassword = makeHash(newUser.password);
+                    user.Password = hasedpassword;
+                    user.Username = newUser.username;
+                    db.Customers.Add(user);
+                    db.SaveChanges();
+                    return RedirectToAction("LogIn");
+                }
+                catch (Exception e)
+                {
+                    return View();
+                }
+            }
+        }
+
+        private static byte[] makeHash(string inPassword)
+        {
+            byte[] inData, outData; 
+            var algorithm = System.Security.Cryptography.SHA256.Create();
+            inData = System.Text.Encoding.ASCII.GetBytes(inPassword);  
+            outData = algorithm.ComputeHash(inData);
+            return outData;
         }
     }
 }
