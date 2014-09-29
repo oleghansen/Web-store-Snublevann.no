54e3da669711ab1ba5c416a12d3416a3860af210
Erik Haider Forsén <erik@forsen.no>
2014-09-29 14:55:27 +0200

    Fikset listing av produkter

--- Nettbutikk/Nettbutikk/Models/DbInitializer.cs
+++ Nettbutikk/Nettbutikk/Models/DbInitializer.cs
@@ -10,23 +10,26 @@ namespace Nettbutikk.Models
     {
         protected override void Seed(DatabaseContext context)
         {
-            var categories = new List<Category>
+            var categories = new List<Categories>
             {
-                new Category {name="Øl"},
-                new Category {name="Vin"},
-                new Category {name="Brennevin"}
+                new Categories {Name="Øl"},
+                new Categories {Name="Vin"},
+                new Categories {Name="Brennevin"}
             };
 
             // add data into context and save to db
 
 
             categories.ForEach(c => context.Categories.Add(c));
-            context.SaveChanges();
 
+            var producers = new List<Producers>
+            {
+                new Producers {Name = "Ringnes"}
+            };
+            producers.ForEach(p => context.Producers.Add(p));
             var products = new List<Products>
             {
-                new Products {Name="Høne", Description="Hei", Price=200}
-
+                new Products {Name="Juleøl", Description="Hei", Price=200,ProducersID=1,CategoriesID=1}
             };
 
             products.ForEach(c => context.Products.Add(c));
