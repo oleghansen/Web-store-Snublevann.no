f473365bdc4353a6cea6871d49461f62547b750b
Erik Haider Forsén <erik@forsen.no>
2014-09-30 14:34:51 +0200

    lagt til view for shoppingcart

--- Nettbutikk/Nettbutikk/Controllers/ShoppingCartController.cs
+++ Nettbutikk/Nettbutikk/Controllers/ShoppingCartController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nettbutikk.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,16 @@ namespace Nettbutikk.Controllers
     public class ShoppingCartController : Controller
     {
         // GET: ShoppingCart
-        public ActionResult Index()
+        public ActionResult viewShoppingCart()
         {
-            return View();
+            if (Session["LoggedInUser"])
+            {
+                Customer c = (Customer)Session["LoggedInUser"];
+                List<ShoppingCartItem> l = c.shoppingcart.shoppingCartItems;
+                return View(l);
+            }
+            else
+                Redirect
         }
     }
 }
