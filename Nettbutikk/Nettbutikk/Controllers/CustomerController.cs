681fb624058268c38a9c964189e4ad87424f08bc
JulieHillRoa <julie.hill.roa@hotmail.com>
2014-10-03 01:38:24 +0200

    liten hack så det ikke kræsjer etter at man har logget inn

--- Nettbutikk/Nettbutikk/Controllers/CustomerController.cs
+++ Nettbutikk/Nettbutikk/Controllers/CustomerController.cs
@@ -29,10 +29,9 @@ namespace Nettbutikk.Controllers
                 bool insertOK = customerDB.add(newUser, hashedPassword);
                 if (insertOK)
                 {
-                    var founduser = customerDB.findCustomer(newUser.username);
-                    Session["loggedInUser"] = founduser;
-                    return View("PersonalSite", founduser);
-                }    
+                    logInUser(newUser.username);
+                    
+                }
             }
             return View();
         }
@@ -78,11 +77,10 @@ namespace Nettbutikk.Controllers
             {
                 //TODO: ugly hack, fikse bedre metode for å få id inn i shoppingcart
              //   var userid = customerDB.findCustomer(user.username).Id;
-                var founduser = customerDB.findCustomer(un);
-                founduser.shoppingcart = new ShoppingCart(founduser.id);
-                Session["loggedInUser"] = founduser;
+                 logInUser(un);
                 ViewBag.loggedIn = true;
-                return View("PersonalSite", founduser);
+                return PersonalSite(); 
+               
             }
             else
             {
@@ -95,13 +93,14 @@ namespace Nettbutikk.Controllers
         //Dersom man går inn i den personlige siden så viser man denne siden, ellers må man logge inn.
         public ActionResult PersonalSite()
         {
+            Debug.WriteLine("tullball");
             if (Session["loggedInUser"] != null )
             {
+                Customer c = (Customer)Session["loggedInUser"];
+                return View("PersonalSite",c);
                
-                return View();
-                
            }
-             return RedirectToAction("LogIn");
+            return View("../Main/Frontpage");
         }
 
         //Hasher passordet
@@ -124,14 +123,46 @@ namespace Nettbutikk.Controllers
         [HttpPost]
         public ActionResult updateUserinfo(Customer newUser)
         {
+           
+
             if (ModelState.IsValid)
             {
-                Customer hm = newUser;
                 Customer c = (Customer)Session["loggedInUser"];
-                    return View("PersonalSite", c);
+                c.firstname = newUser.firstname;
+                c.lastname = newUser.lastname;
+                c.email = newUser.email;
+                c.phonenumber = newUser.phonenumber;
+                c.address = newUser.address;
+                c.postalcode = newUser.postalcode;
+                c.postalarea = newUser.postalarea;
+                c.username = newUser.username;
+                c.password = newUser.password;
+
+                var customerDB = new DBCustomer();
+                byte[] hashedPassword = makeHash(newUser.password);
+                bool updateOK = customerDB.update(c.id,c, newUser.hashpassword);
                 
+                if (updateOK)
+                {
+                    Session["loggedInUser"] = c;
+                    return View("PersonalSite",c);
+                }
+                else
+                {
+                   Customer old = (Customer)Session["loggedInUser"];
+                    return View("PersonalSite",old);
+                }
             }
             return View("../Main/Frontpage");
+        }
+
+        public void logInUser(String un)
+        {
+            var customerDB = new DBCustomer(); 
+            var founduser = customerDB.findCustomer(un);
+            founduser.shoppingcart = new ShoppingCart(founduser.id);
+            Session["loggedInUser"] = founduser;
+            
         }
 
 
