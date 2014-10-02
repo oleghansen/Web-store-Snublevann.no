4af9e3c02f5b7667dea67b86b2748fb29a1d97e1
JulieHillRoa <julie.hill.roa@hotmail.com>
2014-10-02 18:31:54 +0200

    Regex på bruker

--- Nettbutikk/Nettbutikk/Models/Customer.cs
+++ Nettbutikk/Nettbutikk/Models/Customer.cs
@@ -12,34 +12,34 @@ namespace Nettbutikk.Models
         public int id { get; set; }
         [Display(Name = "Fornavn")]
         [Required(ErrorMessage = "Fornavn må oppgis")]
-        [RegularExpression(@"(^[a-zA-Z]+$)", ErrorMessage = "Ugyldig fornavn")]
+        [RegularExpression(@"(^[a-zA-Z\s]+$)", ErrorMessage = "Ugyldig fornavn")]
         public String firstname { get; set; }
         [Display(Name = "Etternavn")]
         [Required(ErrorMessage = "Etternavn må oppgis")]
-        [RegularExpression(@"(^[a-zA-Z]+$)", ErrorMessage = "Ugyldig etternavn")]
+        [RegularExpression(@"(^[a-zA-Z\s]+$)", ErrorMessage = "Ugyldig etternavn")]
         public String lastname { get; set; }
         [Display(Name = "Email")]
         [Required(ErrorMessage = "Email må oppgis")]
         [RegularExpression(@"(^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$)", ErrorMessage = "Ugyldig email")]
         public String email { get; set; }
          [Display(Name = "Telefonnummer")]
-         [RegularExpression(@"(^[0-9\+\(\)])", ErrorMessage = "Ugyldig telefonnummer")]
+         [RegularExpression(@"(^[0-9\+\(\)]+$)", ErrorMessage = "Ugyldig telefonnummer")]
         public String phonenumber { get; set; }
         [Display(Name = "Adresse")]
-        [RegularExpression(@"(^[a-zA-Z])", ErrorMessage = "Ugyldig adresse")]
+        [RegularExpression(@"(^[a-zA-Z0-9\s]+$)", ErrorMessage = "Ugyldig adresse")]
         public String address { get; set; }
         [Display(Name = "Postnummer")]
         [RegularExpression(@"(^[0-9]{4})", ErrorMessage = "Ugyldig postnummer")]
         public int postalcode { get; set; }
         [Display(Name = "Poststed")]
-        [RegularExpression(@"(^[a-zA-Z])", ErrorMessage = "Ugyldig poststed")]
+        [RegularExpression(@"(^[a-zA-Z]+$)", ErrorMessage = "Ugyldig poststed")]
         public String postalarea { get; set; }
         [Display(Name = "Brukernavn")]
-        [RegularExpression(@"(^[a-zA-Z0-9])", ErrorMessage = "Ugyldig brukernavn")]
+        [RegularExpression(@"(^[a-zA-Z0-9_]+$)", ErrorMessage = "Ugyldig brukernavn")]
         [Required(ErrorMessage = "Brukernavn må oppgis")]
         public String username { get; set; }
         [Display(Name = "Passord")]
-        [RegularExpression(@"(^[a-zA-Z0-9])", ErrorMessage = "Ugyldig passord")]
+        [RegularExpression(@"(^[a-zA-Z0-9]+$)", ErrorMessage = "Ugyldig passord")]
         [Required(ErrorMessage = "Passord må oppgis")]
         public String password { get; set; }
 
