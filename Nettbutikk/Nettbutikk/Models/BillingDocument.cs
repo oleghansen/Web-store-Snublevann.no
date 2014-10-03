335fc3bd86be05478faf10a6419f614708379a9f
Erik Haider Forsén <erik@forsen.no>
2014-10-03 14:36:01 +0200

    lagt til fil

---
+++ Nettbutikk/Nettbutikk/Models/BillingDocument.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nettbutikk.Models
+{
+    public class BillingDocument
+    {
+        public Customer customer { get; set; }
+        public Order order { get; set; }
+        public ShoppingCart shoppingcart { get; set; }
+        public double exmva { get; set; }
+        public double mva { get; set; }
+        public int sum { get; set; }
+
+
+    }
+}
