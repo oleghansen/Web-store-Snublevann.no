35438001ca80f1d328cdee7a9d18698dd808dba9
olehansen92 <olehansen92@gmail.com>
2014-10-16 14:05:51 +0200

    Bootstrap + BLL

--- Nettbutikk/Model/OrderLine.cs
+++ Nettbutikk/Model/OrderLine.cs
@@ -8,7 +8,7 @@ namespace Nettbutikk.Model
 {
     public class OrderLine
     {
-        public int id { get; set; }
+        public int id { get; set; } 
         public int productid { get; set; }
         public Product product { get; set; }
         public int quantity { get; set; }
