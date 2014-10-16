9de2f11ef9434d5eb855e48092c23a50e59393c6
Erik Haider Forsén <erik@forsen.no>
2014-10-16 17:11:20 +0200

    All green!

---
+++ Nettbutikk/admin.Tests/ProductControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace admin.Tests
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void Product_list_is_not_null()
+        {
+
+        }
+    }
+}
---
+++ Nettbutikk/admin/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListProducts()
+        {
+            return View();
+        }
+    }
+}
