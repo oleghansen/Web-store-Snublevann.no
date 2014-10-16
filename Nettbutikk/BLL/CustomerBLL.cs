f099466cf39e111ce8549978bd0550099a807c50
olehansen92 <olehansen92@gmail.com>
2014-10-16 15:04:14 +0200

    julie syns den skal være med

---
+++ Nettbutikk/BLL/CustomerBLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Nettbutikk.DAL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nettbutikk.Model;
+
+namespace Nettbutikk.BLL
+{
+    public class CustomerBLL
+    {
+        private ICustomerDAL _customer;
+
+        public CustomerBLL()
+        {
+            _customer = new CustomerDAL();
+        }
+        public CustomerBLL(ICustomerDAL stub)
+        {
+            _customer = stub;
+        }
+
+        public List<Customer> getAll()
+        {
+            List<Customer> allCustomers = _customer.getAll();
+            return allCustomers;
+        }
+    }
+}
