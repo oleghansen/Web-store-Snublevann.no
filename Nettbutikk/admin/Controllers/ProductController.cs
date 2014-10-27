e32058fcaaf5ee3cdd005e7473d4f1d15c0290b2
Erik Haider Forsén <erik@forsen.no>
2014-10-27 14:38:43 +0100

    Endret litt på søk

--- Nettbutikk/admin/Controllers/ProductController.cs
+++ Nettbutikk/admin/Controllers/ProductController.cs
@@ -31,7 +31,7 @@ namespace Nettbutikk.admin.Controllers
             return View();
         }
         
-        public ActionResult ListProducts(int? page, int? itemsPerPage, string sortOrder)
+        public ActionResult ListProducts(int? page, int? itemsPerPage, string sortOrder, string currentFilter, string searchString)
         {
             if (!isAdmin())
                 return RedirectToAction("Main", "Main");
@@ -40,7 +40,18 @@ namespace Nettbutikk.admin.Controllers
             ViewBag.NameSortParm = sortOrder == "Name" ? "name_desc" : "Name";
             ViewBag.PriceSortParm = sortOrder == "Price" ? "price_desc" : "Price";
             ViewBag.ProducerSortParm = sortOrder == "Producer" ? "producer_desc" : "Producer";
-            var allProducts = _product.getAll(null);
+
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter; 
+
+            ViewBag.CurrentFilter = searchString; 
+            List<Product> allProducts; 
+            if(!String.IsNullOrEmpty(searchString))
+                allProducts = _product.getResult(null, searchString); 
+            else
+               allProducts = _product.getAll(null);
 
             switch (sortOrder)
             {
@@ -99,54 +110,7 @@ namespace Nettbutikk.admin.Controllers
             return View(list.ToPagedList(pageNumber: page ?? 1, pageSize: itemsPerPage ?? 10));
         }
       
-        public ActionResult Search(int? page, string searchString)
-        {
-            ProductMenu returnValue = new ProductMenu();
-            if (!isAdmin())
-                return RedirectToAction("Main", "Main");
-
-            int pageNumber = page ?? 1;
-            int itemsPerPage = 25;
-            List<Product> allProducts = _product.getResult(page, searchString);
-            List<ProductInfo> list = new List<ProductInfo>();
-           
-            foreach (var item in allProducts)
-            {
-                if (item.name.ToUpper().Contains(searchString.ToUpper()) || item.description.ToUpper().Contains(searchString.ToUpper()))
-                {
-                    ProductInfo produkt = new ProductInfo()
-                    {
-                        itemnumber = item.itemnumber,
-                        name = item.name,
-                        description = item.description,
-                        category = item.category,
-                        subCategory = item.subCategory,
-                        country = item.country,
-                        price = item.price,
-                        producer = item.producer,
-                        volum = item.volum,
-                        longDescription = item.longDescription,
-                        pricePerLitre = item.pricePerLitre
-                    };
-
-                    list.Add(produkt);
-                }
-            }
-            returnValue.productInfo = list;
-
-            returnValue.categories = new List<CategoryViewModel>();
-            List<Category> allCategories = _product.getAllCategories();
-            foreach (var c in allCategories)
-            {
-                returnValue.categories.Add(new CategoryViewModel()
-                {
-                    SelectedCategoryId = c.ID,
-                    CategoriesName = c.name
-                });
-            }
-
-            return View(list.ToPagedList(pageNumber, itemsPerPage));
-        }
+ 
       
         private bool isAdmin()
         {
