388f8dda4c22ba3f293496209177e9f9971388ef
JulieHillRoa <julie.hill.roa@hotmail.com>
2014-10-27 14:39:35 +0100

    opprettet nye klasser

---
+++ Nettbutikk/admin/Models/OrderLineViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nettbutikk.Models
+{
+    public class OrderLineViewModel
+    {
+     
+    }
+}
