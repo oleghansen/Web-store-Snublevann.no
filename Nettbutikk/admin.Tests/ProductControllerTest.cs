0408574ae039e0546d83d53cc12be1d55fc40a30
snuskeerlend <erlendrog@gmail.com>
2014-11-02 12:52:15 +0100

    product_list_products_name_desc()

--- Nettbutikk/admin.Tests/ProductControllerTest.cs
+++ Nettbutikk/admin.Tests/ProductControllerTest.cs
@@ -65,14 +65,17 @@ namespace Nettbutikk.admin.Tests
 
             var controller = new ProductController(new ProductBLL(new ProductDALStub()));
             builder.InitializeController(controller);
-            builder.HttpContext.Session["loggedInUser"] = new Customer() { admin = true };
+            builder.HttpContext.Session["loggedInUser"] = new Customer() { id=1, admin = true };
 
             // Act
             var action = (ViewResult)controller.ListProducts(2, 2, "name_desc", null, null);
             var result = (IPagedList<ProductInfo>)action.Model;
 
             // Assert
-            Assert.IsTrue(string.Compare(result[0].name, result[1].name) > 0);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.PageNumber);
+            Assert.IsTrue(string.Compare(result[0].name, result[1].name) > 0);    
         }
+
     }
 }
