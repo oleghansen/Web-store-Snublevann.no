2a6c5565b2274a556631ae7d9d9ef5718976a7fb
snuskeerlend <erlendrog@gmail.com>
2014-11-02 12:56:50 +0100

    product_list_products_name()

--- Nettbutikk/admin.Tests/ProductControllerTest.cs
+++ Nettbutikk/admin.Tests/ProductControllerTest.cs
@@ -77,5 +77,27 @@ namespace Nettbutikk.admin.Tests
             Assert.IsTrue(string.Compare(result[0].name, result[1].name) > 0);    
         }
 
+        [TestMethod]
+        public void product_list_products_name()
+        {
+            // Arrange
+            TestControllerBuilder builder = new TestControllerBuilder();
+
+            var controller = new ProductController(new ProductBLL(new ProductDALStub()));
+            builder.InitializeController(controller);
+            builder.HttpContext.Session["loggedInUser"] = new Customer() { id = 1, admin = true };
+
+            // Act
+            var action = (ViewResult)controller.ListProducts(2, 2, "Name", null, null);
+            var result = (IPagedList<ProductInfo>)action.Model;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.PageNumber);
+            Assert.IsTrue(string.Compare(result[0].name, result[1].name) < 0);
+        }
+
+        
+
     }
 }
