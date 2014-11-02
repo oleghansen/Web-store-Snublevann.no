d8bf0585f413f24a220452b1ad42f0e1b58eb677
snuskeerlend <erlendrog@gmail.com>
2014-11-02 18:04:50 +0100

    main_login_view()

--- Nettbutikk/admin.Tests/MainControllerTest.cs
+++ Nettbutikk/admin.Tests/MainControllerTest.cs
@@ -1,23 +1,30 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MvcContrib.TestHelper;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nettbutikk.Model;
+using System.Collections.Generic;
 using Nettbutikk.admin.Controllers;
 using Nettbutikk.BLL;
 using Nettbutikk.DAL;
-using Nettbutikk.Model;
-using PagedList;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Mvc;
+using MvcContrib.TestHelper;
+using Nettbutikk.admin.Models;
+using PagedList;
 
-namespace Nettbutikk.Tests
+namespace Nettbutikk.admin.Tests
 {
     [TestClass]
-
-    class MainControllerTest
+    public class MainControllerTest
     {
+        [TestMethod]
+        public void main_login_view()
+        {
+            // Arrange 
+            var controller = new MainController(new CustomerBLL(new CustomerDALStub()));
+            // Act
+            var result = (ViewResult)controller.LogIn();
 
+            //Assert
+            Assert.AreEqual(result.ViewName, "");
+        }
     }
 }
