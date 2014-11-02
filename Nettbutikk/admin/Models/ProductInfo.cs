e3009189591b99c0dbd74a4cef245c74f1ccf8e6
olehansen92 <olehansen92@gmail.com>
2014-11-02 20:36:38 +0100

    Sorteringsnavn og volumsortering

--- Nettbutikk/admin/Models/ProductInfo.cs
+++ Nettbutikk/admin/Models/ProductInfo.cs
@@ -9,18 +9,31 @@ namespace Nettbutikk.admin.Models
 {
     public class ProductInfo
     {
+        [Display(Name = "Varenummer")]
         public int itemnumber { get; set; }
+        [Display(Name = "Navn")]
         public String name { get; set; }
+        [Display(Name = "Beskrivelse")]
         public String description { get; set; }
+        [Display(Name = "Lang beskrivelse")]
         public String longDescription { get; set; }
+        [Display(Name = "Pris (kr)")]
         public int price { get; set; }
+        [Display(Name = "Volum (cl)")]
         public double volum { get; set; }
+        [Display(Name = "Pris per liter")]
         public double pricePerLitre { get; set; }
+        [Display(Name = "Produsent")]
         public String producer { get; set; }
+        [Display(Name = "Kategori")]
         public String category { get; set; }
+        [Display(Name = "Subkategori")]
         public String subCategory { get; set; }
+        [Display(Name = "Subkategori ID")]
         public int subCategoryid { get; set; }
+        [Display(Name = "Land")]
         public String country { get; set; }
+        [Display(Name = "Kategori ID")]
         public int categoryid { get; set; }
 
     }
